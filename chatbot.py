c185ad71cd64da2a69e465cbaf0ea0d00038ebda
dpranathi9 <dpranathi99@gmail.com>
2025-04-30 12:55:12 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -8,7 +8,7 @@ query_params = st.experimental_get_query_params()
 name = query_params.get("name", ["Friend"])[0]  # default to "Friend"
 
 # Greet the user
-st.title(f"Hi 👋 {name}! Hello/Namaskar/Aadab")
+st.title(f"Hi 👋 Welcome to the News Chatbot"")
 
 # NewsAPI Key
 NEWS_API_KEY = "bf6fdce177a740e19ed32670ec9cd89b"  # Replace this with your actual API key
@@ -21,7 +21,7 @@ def get_top_headlines(api_key):
     return [(a["title"], a["url"]) for a in articles]
     
 if name:
-    greeting = f"Welcome to the news chatbot"
+    #greeting = f"Hi {name}!" if name.lower() != "friends" else "Hi friends!"
     st.subheader(greeting)
 
     st.write("Here are today's top news headlines:")
